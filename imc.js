f881865e7422e0020e84c5feb09e7091151bdfb2
unknown <josecatanaoneto@gmail.com>
2019-12-16 08:53:34 -0300

    o site estar proximo da versão v1.0

--- imc.js
+++ imc.js
@@ -29,12 +29,16 @@ $('#calcular').click(function(){
     console.log('imc :'+imc);
     if(imc<18.5 && peso < 53.5){
         $('#magreza').css('background','red');
+        $('#receitas').attr('href', 'receitas_abaixo_do_peso.html');
     }else if(imc>=18.5 && imc<=24.9 && peso>=58.5 && peso<=72){
         $('#normal').css('background','green');
+        $('#receitas').attr('href', 'receitas_peso_normal.html');
     }else if(imc>=24.9 && imc<=30 && peso>=78.9 && peso<=95.1){
         $('#sobrePeso').css('background','yellow');
+        $('#receitas').attr('href', 'receitas_acima_do_peso.html');
     }else{
         $('#obesidade').css('background','red');
+        $('#receitas').attr('href', 'receitas_acima_do_peso.html');
     }
 
 
